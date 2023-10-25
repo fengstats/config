c3bdbdfa6940c137821b6f2128d30c0b4257b50d
feng <2860984180@qq.com>
2023-10-26 01:59:04 +0800

    feat(shell): add note time statistics script

---
+++ 4-shell/noteStats.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title noteStats
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🧑🏻‍💻
+
+# Documentation:
+# @raycast.description 执行日记时长统计 JS 脚本
+# @raycast.author fengstats
+# @raycast.authorURL https://raycast.com/fengstats
+
+node /Users/feng/codebase/config/3-js/note-stats.js /Users/feng/mine/life/daily-note/2023/10月
