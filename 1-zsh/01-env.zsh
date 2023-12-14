05843773849d8bbc636af6c2ce5337e633560c92
fengstats <2860984180@qq.com>
2023-12-14 18:02:35 +0800

    refactor(zsh): delete zsh config

--- 1-zsh/01-env.zsh
+++
@@ -1,5 +0,0 @@
-# 导出环境变量
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-# proxy url
-export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
--- 1-zsh/vi.zsh
+++
@@ -1,3 +0,0 @@
-# 我的 zsh-vi-mode 配置文件
-bindkey -M vicmd "H" vi-beginning-of-line
-bindkey -M vicmd "L" vi-end-of-line
